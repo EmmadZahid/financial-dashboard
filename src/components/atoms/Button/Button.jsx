06c1b947ff2316e968f5aa79d6381c4b7b9b38e3
emmadZahid <emmad.zahid@gmail.com>
2024-12-24 00:49:56 +0500

    Fixed the button outline

--- src/components/atoms/Button/Button.jsx
+++ src/components/atoms/Button/Button.jsx
@@ -2,16 +2,15 @@ import clsx from "clsx";
 
 const Button = ({ className, children, ...props }) => {
   const classes = clsx({
-    "bg-gray-800": props.disabled,
-    "bg-black active:opacity-100 active:outline active:outline-2 active:outline-gray-400 hover:opacity-85":
-      !props.disabled,
+    "bg-gray-800 cursor-not-allowed": props.disabled, // Disabled state
+    "bg-black active:opacity-100 active:outline-gray-400 hover:opacity-75 active:ring-2 active:ring-gray-400":
+      !props.disabled, // Styles for active, focus, and hover states
   });
+
   return (
-    <>
-      <button {...props} className={`${className} ${classes} text-white `}>
-        {children}
-      </button>
-    </>
+    <button {...props} className={`${className} ${classes} text-white`}>
+      {children}
+    </button>
   );
 };
 
